720dd7ae8b7862c82b8fd3af52b01baf133acba8
joyjames143 <joyjames143007@gmail.com>
2021-10-06 19:26:56 +0530

    addedspace

--- src/components/Showanswer.js
+++ src/components/Showanswer.js
@@ -19,7 +19,7 @@ export default function Showanswer({qnsNum,totalQns,specificQns,userRememberArra
             <div className="showanswer-questions-main-div">
                     <div className="showanswer-question-test-div">
                         
-                        <h2> {qnsNum} . 
+                        <h2> {qnsNum}&nbsp; .&nbsp; 
                         <MathJaxContext version={3} config={config}>
                                 <MathJax inline dynamic>{specificQns.questionText}</MathJax>
                         </MathJaxContext>
